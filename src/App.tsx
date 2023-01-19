4411e4264834659afa71e6a8e288aa083d3b8770
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-01-19 09:33:53 -0300

    feat: create header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
+import { Header } from './components/Header';
 import './styles/global.css';
 
-import { Habit } from './components/Habit';
-
-function App() {
+export function App() {
   return (
-    <div className="App">
-      <Habit completed={21} />
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+      </div>
     </div>
   );
 }
-
-export default App;
