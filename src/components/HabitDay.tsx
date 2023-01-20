81f085d8b4523b3e279bebd112076eef1da1aace
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-01-20 20:21:20 -0300

    feat: custom habit day color by progress

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -1,17 +1,40 @@
 import * as Popover from '@radix-ui/react-popover';
+import clsx from 'clsx';
 import { ProgressBar } from './ProgressBar';
 
-export function HabitDay() {
+interface HabitDayProps {
+  amount: number;
+  completed: number;
+}
+
+export function HabitDay(props: HabitDayProps) {
+  const completedPercentage = Math.round(
+    (props.completed / props.amount) * 100
+  );
+
   return (
     <Popover.Root>
-      <Popover.Trigger className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg" />
+      <Popover.Trigger
+        className={clsx('w-10 h-10 border-2 rounded-lg', {
+          'bg-zinc-900 border-zinc-800': completedPercentage === 0,
+          'bg-teal-900 border-teal-800':
+            completedPercentage > 0 && completedPercentage < 20,
+          'bg-teal-800 border-teal-600':
+            completedPercentage >= 20 && completedPercentage < 40,
+          'bg-teal-600 border-teal-500':
+            completedPercentage >= 40 && completedPercentage < 60,
+          'bg-teal-500 border-teal-400':
+            completedPercentage >= 60 && completedPercentage < 80,
+          'bg-teal-400 border-teal-300': completedPercentage >= 80,
+        })}
+      />
 
       <Popover.Portal>
         <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
           <span className="font-semibold text-zinc-400">Friday</span>
           <span className="mt-1 font-bold leading-tight text-3xl">20/01</span>
 
-          <ProgressBar progress={25} />
+          <ProgressBar progress={completedPercentage} />
 
           <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
         </Popover.Content>
