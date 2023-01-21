e87e47da2bd6ff83f004a266c077786056f4a05e
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-01-21 10:24:06 -0300

    feat: create checkbox component

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -1,6 +1,8 @@
 import * as Popover from '@radix-ui/react-popover';
+import * as Checkbox from '@radix-ui/react-checkbox';
 import clsx from 'clsx';
 import { ProgressBar } from './ProgressBar';
+import { Check } from 'phosphor-react';
 
 interface HabitDayProps {
   amount: number;
@@ -35,6 +37,19 @@ export function HabitDay(props: HabitDayProps) {
           <span className="mt-1 font-bold leading-tight text-3xl">20/01</span>
 
           <ProgressBar progress={completedPercentage} />
+
+          <div className="mt-6 flex flex-col gap-3">
+            <Checkbox.Root className="flex items-center gap-3 group">
+              <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-blue-600 group-data-[state=checked]:border-blue-600">
+                <Checkbox.Indicator>
+                  <Check size={20} className="text-white" />
+                </Checkbox.Indicator>
+              </div>
+              <span className="font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400">
+                Drink water
+              </span>
+            </Checkbox.Root>
+          </div>
 
           <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
         </Popover.Content>
--- src/components/NewHabitForm.tsx
+++ src/components/NewHabitForm.tsx
@@ -1,4 +1,15 @@
 import { Check } from 'phosphor-react';
+import * as Checkbox from '@radix-ui/react-checkbox';
+
+const weekDaysNames = [
+  'Sunday',
+  'Monday',
+  'Tueday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+];
 
 export function NewHabitForm() {
   return (
@@ -16,6 +27,21 @@ export function NewHabitForm() {
       <label htmlFor="" className="font-semibold leading-tight mt-4">
         Which days of the week?
       </label>
+      <div className="mt-3 flex flex-col gap-2">
+        {weekDaysNames.map(day => {
+          return (
+            <Checkbox.Root key={day} className="flex items-center gap-3 group">
+              <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-blue-600 group-data-[state=checked]:border-blue-600">
+                <Checkbox.Indicator>
+                  <Check size={20} className="text-white" />
+                </Checkbox.Indicator>
+              </div>
+              <span className="text-white leading-tight ">{day}</span>
+            </Checkbox.Root>
+          );
+        })}
+      </div>
+
       <button
         type="submit"
         className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-teal-700 hover:bg-teal-600"
