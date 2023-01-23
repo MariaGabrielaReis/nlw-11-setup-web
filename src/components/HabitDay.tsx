a59b23214234d577a80db88560df1c3382f495a8
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-01-23 19:33:32 -0300

    feat: list day habits

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -1,9 +1,8 @@
 import * as Popover from '@radix-ui/react-popover';
-import * as Checkbox from '@radix-ui/react-checkbox';
 import clsx from 'clsx';
 import { ProgressBar } from './ProgressBar';
-import { Check } from 'phosphor-react';
 import dayjs from 'dayjs';
+import { HabitsList } from './HabitsList';
 
 interface HabitDayProps {
   date: Date;
@@ -46,18 +45,7 @@ export function HabitDay({ date, amount = 0, completed = 0 }: HabitDayProps) {
 
           <ProgressBar progress={completedPercentage} />
 
-          <div className="mt-6 flex flex-col gap-3">
-            <Checkbox.Root className="flex items-center gap-3 group">
-              <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-blue-600 group-data-[state=checked]:border-blue-600">
-                <Checkbox.Indicator>
-                  <Check size={20} className="text-white" />
-                </Checkbox.Indicator>
-              </div>
-              <span className="font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400">
-                Drink water
-              </span>
-            </Checkbox.Root>
-          </div>
+          <HabitsList date={date} />
 
           <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
         </Popover.Content>
---
+++ src/components/HabitsList.tsx
@@ -0,0 +1,54 @@
+import * as Checkbox from '@radix-ui/react-checkbox';
+import dayjs from 'dayjs';
+import { Check } from 'phosphor-react';
+import { useEffect, useState } from 'react';
+import { api } from '../lib/axios';
+
+interface HabitListProps {
+  date: Date;
+}
+
+interface HabitsInfo {
+  possibleHabits: Array<{ id: string; title: string; created_ad: string }>;
+  completedHabits: string[];
+}
+
+export function HabitsList({ date }: HabitListProps) {
+  const [habitsInfo, setHabitsInfo] = useState<HabitsInfo>();
+  const isDateInPast = dayjs(date).endOf('day').isBefore(new Date());
+
+  async function getHabitsList() {
+    const { data } = await api.get('/day', {
+      params: {
+        date: date.toISOString(),
+      },
+    });
+    setHabitsInfo(data);
+  }
+
+  useEffect(() => {
+    getHabitsList();
+  }, []);
+
+  return (
+    <div className="mt-6 flex flex-col gap-3">
+      {habitsInfo?.possibleHabits.map(habit => (
+        <Checkbox.Root
+          key={habit.id}
+          checked={habitsInfo.completedHabits.includes(habit.id)}
+          disabled={isDateInPast}
+          className="flex items-center gap-3 group"
+        >
+          <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-blue-600 group-data-[state=checked]:border-blue-600">
+            <Checkbox.Indicator>
+              <Check size={20} className="text-white" />
+            </Checkbox.Indicator>
+          </div>
+          <span className="font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400">
+            {habit.title}
+          </span>
+        </Checkbox.Root>
+      ))}
+    </div>
+  );
+}
